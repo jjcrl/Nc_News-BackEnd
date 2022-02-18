91d2632bfd296c460184b6646a92abdde853eef3
jjcrl <josephcarroll@Josephs-MacBook-Pro.local>
2022-02-18 10:47:17 +0000

    updates to comment count testing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -237,15 +237,16 @@ describe("ENDPOINT TESTING", () => {
           .expect(200)
           .then((response) => {
             expect(response.body.articles.length).toBe(1);
-            expect(response.body.articles[0]).toEqual({
-              title: "UNCOVERED: catspiracy to bring down democracy",
-              topic: "cats",
-              author: "rogersop",
-              created_at: "2020-08-03T13:14:00.000Z",
-              votes: 0,
-              article_id: 5,
-              comment_count: expect.any(String),
-            });
+            expect(response.body.articles[0]).toEqual(
+              expect.objectContaining({
+                title: "UNCOVERED: catspiracy to bring down democracy",
+                topic: "cats",
+                author: "rogersop",
+                created_at: "2020-08-03T13:14:00.000Z",
+                votes: 0,
+                article_id: 5,
+              })
+            );
           });
       });
       test("200: Should return an empty array when topic exists, but there are no articles associated", () => {
@@ -353,6 +354,23 @@ describe("ENDPOINT TESTING", () => {
             votes: 100,
             comment_count: "11",
           });
+        });
+    });
+    test("200: should still include a comment count witjh a value of zero when an aricle has no comments", () => {
+      return request(app)
+        .get("/api/articles/2")
+        .expect(200)
+        .then((response) => {
+          expect(response.body.article).toEqual({
+            article_id:2,
+            title: "Sony Vaio; or, The Laptop",
+            topic: "mitch",
+            author: "icellusedkars",
+            body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+            created_at: '2020-10-16T05:03:00.000Z',
+            votes: 0,
+            comment_count:'0'
+          },);
         });
     });
   });
