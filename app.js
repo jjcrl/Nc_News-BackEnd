6d530d1b9f65b446e4354f7883607c5144fe5e50
jjcrl <josephcarroll@Josephs-MacBook-Pro.local>
2022-02-20 23:51:04 +0000

    heroku config

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const endpoints = require("./endpoints.json");
 app.use(express.json());
 
 app.get("/api", (req, res) => {
-  res.status(200).send({endpoints});
+  res.status(200).send({ endpoints });
 });
 
 app.get("/api/topics", getAllTopics);
